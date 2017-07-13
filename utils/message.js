f1827f840e1ee356aadf48db9d7a5564a71526f8
zaherab <zaheraboushakra@gmail.com>
2017-07-13 11:00:59 +0400

    add google api key to the image url

--- utils/message.js
+++ utils/message.js
@@ -11,7 +11,7 @@ var generateLocationMessage = (from, lat, long) => {
     return{
         from,
         url:`https://www.google.com/maps?q=${lat},${long}`,
-        imgUrl :`https://maps.googleapis.com/maps/api/staticmap?center=${lat},${long}&size=250x150&zoom=14&scale=1&markers=color:red|lable:S|${lat},${long}`,
+        imgUrl :`https://maps.googleapis.com/maps/api/staticmap?center=${lat},${long}&size=250x150&zoom=14&scale=1&markers=color:red|lable:S|${lat},${long}&key=AIzaSyAYUd8MPnfYjLk_ypiWm8_jTBhUIhRxf6g`,
         createdAt: moment().valueOf(),
     }
 }
